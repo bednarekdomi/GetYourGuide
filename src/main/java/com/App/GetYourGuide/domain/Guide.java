4dbb8f9be9463a6ac451b8b75ea0e0f3975381a1
bednarekdomi <bednarekdomi@gmail.com>
2023-07-14 11:30:32 +0200

    refactored Guide class into Entity

--- src/main/java/com/App/GetYourGuide/domain/Guide.java
+++ src/main/java/com/App/GetYourGuide/domain/Guide.java
@@ -1,8 +1,15 @@
 package com.App.GetYourGuide.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Guide {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
     String name;
     boolean isAvailable;
