563d6cd4698d117340334ffc96e553d247581e33
bednarekdomi <bednarekdomi@gmail.com>
2023-07-17 12:53:21 +0200

    added OrderDetails class

--- src/main/java/com/App/GetYourGuide/domain/Guide.java
+++ src/main/java/com/App/GetYourGuide/domain/Guide.java
@@ -4,14 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Guide {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    long id;
-    String name;
-    boolean isAvailable;
+    private long id;
+    private String name;
+    private boolean isAvailable;
 
 }
---
+++ src/main/java/com/App/GetYourGuide/domain/OrderDetails.java
@@ -0,0 +1,14 @@
+package com.App.GetYourGuide.domain;
+
+import java.util.List;
+
+public class OrderDetails {
+    private Long orderId;
+    private Order order;
+    private Guide guide;
+    private boolean isPaid;
+    private boolean isVerified;
+    private boolean isSubmitted;
+
+
+}
