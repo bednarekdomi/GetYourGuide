98c29cfccad31a37dd3c68d289b5306c3f3d70ea
bednarekdomi <bednarekdomi@gmail.com>
2023-07-20 10:00:08 +0200

    turned OrderDetils class into database entity

--- src/main/java/com/App/GetYourGuide/domain/OrderDetails.java
+++ src/main/java/com/App/GetYourGuide/domain/OrderDetails.java
@@ -1,10 +1,23 @@
 package com.App.GetYourGuide.domain;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long orderId;
+    @OneToOne
     private Order order;
+    @OneToOne
     private Guide guide;
     private boolean isPaid;
     private boolean isVerified;
