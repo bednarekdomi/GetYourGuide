ac47dea6890c6231dfc8e430945e6db578abe15c
bednarekdomi <bednarekdomi@gmail.com>
2023-07-21 13:53:20 +0200

    created GuideDto class

---
+++ src/main/java/com/App/GetYourGuide/domain/GuideDto.java
@@ -0,0 +1,15 @@
+package com.App.GetYourGuide.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class GuideDto {
+
+    private long id;
+    private String name;
+    private boolean isAvailable;
+}
