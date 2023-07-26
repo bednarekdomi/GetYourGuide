21afbbf07dd619d9c86474158a9b3cdb0ff56c50
bednarekdomi <bednarekdomi@gmail.com>
2023-07-26 10:17:42 +0200

    created OrderDetailsRepository interface in Repository package

---
+++ src/main/java/com/App/GetYourGuide/Repository/OrderDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.App.GetYourGuide.Repository;
+
+import com.App.GetYourGuide.domain.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Integer> {
+}
