ddbdb2129c7c30168edf3e08e3850b7b6128e344
bednarekdomi <bednarekdomi@gmail.com>
2023-08-04 10:18:21 +0200

    created createOrder method in OrderDetailsService

--- src/main/java/com/App/GetYourGuide/Service/OrderDetailsService.java
+++ src/main/java/com/App/GetYourGuide/Service/OrderDetailsService.java
@@ -1,11 +1,16 @@
 package com.App.GetYourGuide.Service;
 
-
 import com.App.GetYourGuide.Mapper.OrderDetailsMapper;
 import com.App.GetYourGuide.Repository.OrderDetailsRepository;
+import com.App.GetYourGuide.domain.Guide;
+import com.App.GetYourGuide.domain.OrderDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +19,24 @@ public class OrderDetailsService {
     private final OrderDetailsRepository orderDetailsRepository;
     private final OrderDetailsMapper orderDetailsMapper;
 
+        public void createOrder(LocalDate date) {
+            List<Guide> availableGuide = new ArrayList<>();
+            OrderDetails orderToCreate = new OrderDetails();
+            List<OrderDetails> allOrders = orderDetailsRepository.findAll();
+            for (OrderDetails order : allOrders) {
+                if (order.getTourDate() != date) {
+                    availableGuide.add(order.getGuide());
+                }
+            }
+
+            if (availableGuide.isEmpty()) {
+                System.out.println("No guides available for this day");
+            } else {
+                availableGuide.sort(Comparator.comparingLong(Guide::getDaysOffSinceLastTrip).reversed());
+                orderToCreate.setGuide(availableGuide.get(0));
+                orderToCreate.setTourDate(date);
+                orderDetailsRepository.save(orderToCreate);
+            }
+        }
 
 }
