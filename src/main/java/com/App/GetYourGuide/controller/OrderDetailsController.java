50ef7d69238a61f4f2959b272a2b63812d24b03d
bednarekdomi <bednarekdomi@gmail.com>
2023-08-08 12:33:33 +0200

    added createOrderForTheDate method in orderDetailsController

--- src/main/java/com/App/GetYourGuide/controller/OrderDetailsController.java
+++ src/main/java/com/App/GetYourGuide/controller/OrderDetailsController.java
@@ -2,8 +2,13 @@ package com.App.GetYourGuide.controller;
 
 import com.App.GetYourGuide.Service.OrderDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderDetailsController {
 
     private final OrderDetailsService orderDetailsService;
+
+    @PostMapping("/createOrder")
+    public ResponseEntity<String> createOrderForTheDate(@RequestParam LocalDate date) {
+        orderDetailsService.createOrder(date);
+        return ResponseEntity.ok("Order placed successfully");
+    }
 }
