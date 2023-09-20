16fac0d8f6b233392e8c6d45d4fdab6ead1fcf81
bednarekdomi <bednarekdomi@gmail.com>
2023-09-20 11:18:44 +0200

    added Junit5 dependency

---
+++ src/test/java/com/App/GetYourGuide/service/OrderServiceTest.java
@@ -0,0 +1,35 @@
+package com.App.GetYourGuide.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith
+class OrderServiceTest {
+
+    @Test
+    void getOrder() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void updateOrderDetails() {
+    }
+
+    @Test
+    void refundPayment() {
+    }
+}
