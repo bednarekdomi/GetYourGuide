bc62c9fc8a2287636d038c374da5df9d89a6312f
bednarekdomi <bednarekdomi@gmail.com>
2023-09-20 13:44:04 +0200

    added first test method in OrderServiceTest

--- src/main/java/com/App/GetYourGuide/domain/OrderDto.java
+++ src/main/java/com/App/GetYourGuide/domain/OrderDto.java
@@ -4,15 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class OrderDto {
 
     private Long orderId;
+    private TourOrder tour;
     private GuideDto guideDto;
+    private LocalDate tourDate;
     private boolean isPaid;
     private boolean isVerified;
     private boolean isSubmitted;
     private CustomerDto customerDto;
+    private String tourDescription = tour.getDescription();
+    private double orderCost = tour.getCost();
 }
--- src/test/java/com/App/GetYourGuide/service/OrderServiceTest.java
+++ src/test/java/com/App/GetYourGuide/service/OrderServiceTest.java
@@ -1,16 +1,56 @@
 package com.App.GetYourGuide.service;
 
+import com.App.GetYourGuide.domain.*;
+import com.App.GetYourGuide.mapper.OrderMapper;
+import com.App.GetYourGuide.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@RunWith
+@ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
 
+    @InjectMocks
+    private OrderService orderService;
+    @Mock
+    private OrderMapper orderMapper;
+    @Mock
+    private OrderRepository orderRepository;
+
+    private OrderDto orderOneDto;
+    private Order orderOne;
+
+    @BeforeEach
+    public void setUp(){
+        TourOrder firstOrderDecorator = new InsuranceDecorator(new EquipmentDecorator(new BasicOrderDecorator()));
+        orderOne = new Order(1L, firstOrderDecorator, new Guide(), LocalDate.of(2023, 9, 30), true, true, true,
+                new Customer(), firstOrderDecorator.getDescription(), firstOrderDecorator.getCost());
+        orderOneDto = new OrderDto(1L, firstOrderDecorator, new GuideDto(), LocalDate.of(2023, 9, 30), true,
+                true, true, new CustomerDto(), firstOrderDecorator.getDescription(), firstOrderDecorator.getCost());
+    }
+
     @Test
-    void getOrder() {
+    void shouldGetOrderById() {
+        //Given
+        when(orderMapper.mapToOrderDetailsDto(orderOne)).thenReturn(orderOneDto);
+        when(orderRepository.findById(1L)).thenReturn(Optional.of(orderOne));
+        //When
+        Optional<OrderDto> order = orderService.getOrder(1L);
+        //Then
+        assertTrue(order.isPresent());
+        assertEquals(orderOneDto, order.get());
     }
 
     @Test
