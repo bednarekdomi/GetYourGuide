3a0d1b607f0b23349f66514d47cbd9892b098b8e
bednarekdomi <bednarekdomi@gmail.com>
2024-01-11 15:54:12 +0100

    overrided equals and hastCode methods

--- src/main/java/com/App/GetYourGuide/domain/Order.java
+++ src/main/java/com/App/GetYourGuide/domain/Order.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Orders")
@@ -41,5 +42,29 @@ public class Order {
         this.verified = verified;
         this.submitted = submitted;
         this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+        return isPaid() == order.isPaid() &&
+                isVerified() == order.isVerified() &&
+                isSubmitted() == order.isSubmitted() &&
+                Objects.equals(orderId, order.orderId) &&
+                Objects.equals(tour, order.tour) &&
+                Objects.equals(guide, order.guide) &&
+                Objects.equals(tourDate, order.tourDate) &&
+                Objects.equals(customer, order.customer);
+    }
+
+
+    @Override
+    public int hashCode() {
+        int year = tourDate.getYear() % 100;
+        int month = tourDate.getMonthValue();
+        int day = tourDate.getDayOfYear();
+        return day * 1000000 + month * 10000 + year * 100;
     }
 }
