11c773b953592ea860cdb72a0c1f1e53b9fb9213
bednarekdomi <bednarekdomi@gmail.com>
2024-04-02 15:14:25 +0200

    added 2 new methods - getAvailableGuides, getDaysOffSinceLastTrip

--- src/main/java/com/App/GetYourGuide/controller/GuideController.java
+++ src/main/java/com/App/GetYourGuide/controller/GuideController.java
@@ -1,9 +1,15 @@
 package com.App.GetYourGuide.controller;
 
+import com.App.GetYourGuide.domain.Guide;
 import com.App.GetYourGuide.service.GuideService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class GuideController {
 
     private final GuideService guideService;
+
+    @GetMapping("/getAvailableGuides")
+    public List<Guide> getAvailableGuides(@RequestParam LocalDate date) {
+        return guideService.getAvailableGuides(date);
+    }
+
+    @GetMapping("/getDaysOffSinceLastTrip")
+    public long getDaysOffSinceLastTrip(@RequestParam long guideId) {
+        return guideService.getDaysOffSinceLastTrip(guideId);
+    }
 }
